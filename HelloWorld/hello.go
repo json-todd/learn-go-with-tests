aa9557fdb80831554ab491f45cd152b8c5c30677
Tri Anh Phung <cadbane1998@gmail.com>
2024-07-09 22:22:42 +0300

    test: write test case for default behaviour. create a helper function for test & refactor test cases. feat: make test case for default behaviour green

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -5,9 +5,12 @@ const englishHelloPrefix = "Hello, "
 const finnishHelloPrefix = "Moi, "
 
 func Hello(name string) string {
+    if name == "" {
+        name = "World"
+    }
     return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello(""))
 }
--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello("Tri Anh")
-  want := "Hello, Tri Anh"
+  t.Run("saying hello to people", func(t *testing.T) {
+    got := Hello("Tri Anh")
+    want := "Hello, Tri Anh"
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
+  })
+
+  t.Run("say 'Hello, World' as a default when nothing (emptry string) is the input", func (t *testing.T){
+    got := Hello("")
+    want := "Hello, World"
+
+    if got != want {
+      t.Errorf("got %q want %q", got ,want)
+    }
+  })
 }

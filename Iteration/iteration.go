a8b87162844cd759781e418781aff30e03834da6
Tri Anh Phung <cadbane1998@gmail.com>
2024-07-13 12:15:33 +0300

    test: change happy path test to feeding another argument. feat: make happy path test green

--- Iteration/iteration.go
+++ Iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, amount int) string {
     var repeated string
-    for i := 0; i < 6; i ++ {
+    for i := 0; i < amount; i ++ {
         repeated += character
     }
     return repeated
--- Iteration/iteration_test.go
+++ Iteration/iteration_test.go
@@ -3,10 +3,14 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-    repeated := Repeat("t")
-    expected := "tttttt"
+    t.Run("Passing 6 to function should have the string repeated 6 times ", func (t *testing.T) {
+      repeated := Repeat("t", 6)
+      expected := "tttttt"
 
-    if repeated != expected {
-        t.Errorf("expected %q but got %q", expected, repeated)
-    }
+      if repeated != expected {
+          t.Errorf("expected %q but got %q", expected, repeated)
+      }
+    })
 }
+
+

1f8c6d5f14676e2955b93959b4a9297e7312bf93
Tri Anh Phung <cadbane1998@gmail.com>
2024-07-18 23:44:24 +0300

    test: more test cases to verify behaviors. all pass so no need to write any more codes

--- ArrayNSlices/sum_test.go
+++ ArrayNSlices/sum_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    t.Run("collect of 5 numbers", func(t *testing.T) {
+    t.Run("collection of 5 numbers", func(t *testing.T) {
       numbers := []int{1,2,3,4,5}
 
       got := Sum(numbers)
@@ -23,5 +23,40 @@ func TestSum(t *testing.T) {
         if got != want {
             t.Errorf("got %d want %d given, %v", got, want, numbers)
         }
+    })
+
+    t.Run("an empty slice",func(t *testing.T){
+        numbers := []int{}
+
+        got := Sum(numbers)
+        want := 0
+
+        if got != want {
+            t.Errorf("got %d want %d given, %v", got, want, numbers)
+        }
+    })
+
+
+    t.Run("negative and positive numbers", func(t *testing.T){
+        numbers := []int {-3, -2, -1, 1, 2, 3}
+
+        got := Sum(numbers)
+        want := 0
+
+        if got != want {
+            t.Errorf("got %d want %d given, %v", got, want, numbers)
+        }
+    })
+
+    t.Run("zero are supported in collection", func(t *testing.T){
+        numbers:= []int{0,0,0,0,0,0,0,0,0,0}
+
+        got := Sum(numbers)
+        want := 0
+
+        if got != want {
+            t.Errorf("got %d want %d given, %v", got, want, numbers)
+        }
+
     })
 }

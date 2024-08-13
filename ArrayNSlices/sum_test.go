1a45861dbd4c3ffaf731a9ed8eff213ea1f81671
Tri Anh Phung <cadbane1998@gmail.com>
2024-08-13 20:51:17 +0300

    [test] write first test for new function

--- ArrayNSlices/sum_test.go
+++ ArrayNSlices/sum_test.go
@@ -71,3 +71,11 @@ func TestSumAll(t *testing.T) {
     }
 }
 
+func TestSumTail(t *testing.T) {
+		got := SumAllTail([]int{1,2}, []int{0,9})
+		want := []int{2,9}
+
+		if !reflect.DeepEqual(got,want) {
+				t.Errorf("got %v want %v", got, want)
+		}
+}
